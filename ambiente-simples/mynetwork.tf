af1b06e963afb56526b3980e33c1468fe024e2d7
Daniel Amaral <eumagnun@gmail.com>
2022-10-26 15:39:16 -0300

    Add files via upload

---
+++ ambiente-simples/mynetwork.tf
@@ -0,0 +1,55 @@
+# Create the mynetwork network
+resource "google_compute_network" "mynetwork" {
+  name                    = "mynetwork"
+  auto_create_subnetworks = false
+}
+
+
+resource "google_compute_firewall" "mynetwork-allow-http" {
+  name    = "mynetwork-allow-http"
+  network = google_compute_network.mynetwork.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["web"]
+}
+
+resource "google_compute_firewall" "mynetwork-allow-database" {
+  name    = "mynetwork-allow-database"
+  network = google_compute_network.mynetwork.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  target_tags = ["database"]
+  source_tags = ["web"]
+}
+
+resource "google_compute_subnetwork" "subnet-southamerica-east1" {
+  name          = "subnet-southamerica-east1"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "southamerica-east1"
+  network       = google_compute_network.mynetwork.id
+}
+
+
+module "backend-vm" {
+  source           = "./instance"
+  instance_name    = "backend-vm"
+  instance_zone    = "southamerica-east1"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+
+
+module "database-vm" {
+  source           = "./instance"
+  instance_name    = "database-vm"
+  instance_zone    = "southamerica-east1"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+
