2dfb9ba2304eee6968885ff96b7c9e7f37260d6e
Daniel da Silva Amaral <eumagnun@gmail.com>
2022-11-23 09:59:50 -0300

    Update mynetwork.tf

--- ambiente-simples/mynetwork.tf
+++ ambiente-simples/mynetwork.tf
@@ -61,6 +61,28 @@ resource "google_compute_subnetwork" "subnet-southamerica-east1" {
   network       = google_compute_network.mynetwork.id
 }
 
+resource "google_compute_router" "my-router" {
+  name    = "my-router"
+  region  = google_compute_subnetwork.subnet-southamerica-east1.region
+  network = google_compute_network.mynetwork.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "my-router-nat" {
+  name                               = "my-router-nat"
+  router                             = google_compute_router.my-router.name
+  region                             = google_compute_router.my-router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
 
 module "backend-vm" {
   source           = "./instance"
