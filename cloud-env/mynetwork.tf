ef22830fab42802a415303d2746478aa0daefcf3
Daniel da Silva Amaral <eumagnun@gmail.com>
2022-11-24 13:55:46 -0300

    Update mynetwork.tf

--- cloud-env/mynetwork.tf
+++ cloud-env/mynetwork.tf
@@ -1,13 +1,13 @@
-# Create the mynetwork network
-resource "google_compute_network" "mynetwork" {
-  name                    = "mynetwork"
+# Create the mynetwork1 network
+resource "google_compute_network" "mynetwork1" {
+  name                    = "mynetwork1"
+  routing_mode            = "GLOBAL"
   auto_create_subnetworks = false
 }
 
-
-resource "google_compute_firewall" "mynetwork-allow-http" {
-  name    = "mynetwork-allow-http"
-  network = google_compute_network.mynetwork.self_link
+resource "google_compute_firewall" "mynetwork1-allow-http" {
+  name    = "mynetwork1-allow-http"
+  network = google_compute_network.mynetwork1.self_link
 
   allow {
     protocol = "tcp"
@@ -17,9 +17,9 @@ resource "google_compute_firewall" "mynetwork-allow-http" {
   target_tags   = ["web"]
 }
 
-resource "google_compute_firewall" "mynetwork-allow-database" {
-  name    = "mynetwork-allow-database"
-  network = google_compute_network.mynetwork.self_link
+resource "google_compute_firewall" "mynetwork1-allow-database" {
+  name    = "mynetwork1-allow-database"
+  network = google_compute_network.mynetwork1.self_link
 
   allow {
     protocol = "tcp"
@@ -30,9 +30,9 @@ resource "google_compute_firewall" "mynetwork-allow-database" {
   source_tags = ["web"]
 }
 
-resource "google_compute_firewall" "mynetwork-allow-ssh-from-build-vm" {
-  name    = "mynetwork-allow-ssh-from-build-vm"
-  network = google_compute_network.mynetwork.self_link
+resource "google_compute_firewall" "mynetwork1-allow-ssh-from-build-vm" {
+  name    = "mynetwork1-allow-ssh-from-build-vm"
+  network = google_compute_network.mynetwork1.self_link
 
   allow {
     protocol = "tcp"
@@ -42,9 +42,9 @@ resource "google_compute_firewall" "mynetwork-allow-ssh-from-build-vm" {
   source_tags = ["build"]
 }
 
-resource "google_compute_firewall" "mynetwork-allow-ssh-from-cloudshell" {
-  name    = "mynetwork-allow-ssh-from-cloudshell"
-  network = google_compute_network.mynetwork.self_link
+resource "google_compute_firewall" "mynetwork1-allow-ssh-from-cloudshell" {
+  name    = "mynetwork1-allow-ssh-from-cloudshell"
+  network = google_compute_network.mynetwork1.self_link
 
   allow {
     protocol = "tcp"
@@ -58,13 +58,13 @@ resource "google_compute_subnetwork" "subnet-southamerica-east1" {
   name          = "subnet-southamerica-east1"
   ip_cidr_range = "10.0.0.0/24"
   region        = "southamerica-east1"
-  network       = google_compute_network.mynetwork.id
+  network       = google_compute_network.mynetwork1.id
 }
 
 resource "google_compute_router" "my-router" {
   name    = "my-router"
   region  = google_compute_subnetwork.subnet-southamerica-east1.region
-  network = google_compute_network.mynetwork.id
+  network = google_compute_network.mynetwork1.id
 
   bgp {
     asn = 64514
@@ -103,42 +103,22 @@ resource "google_storage_bucket" "frontend-static-site" {
   }
 }
 
-resource "google_sql_database_instance" "main" {
-  name             = "main"
-  database_version = "POSTGRES_14"
-  region           = "southamerica-east1"
-
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = "db-f1-micro"
-  }
-}
-
 module "backend-vm" {
   source           = "./instance"
   instance_name    = "backend-vm"
   instance_zone    = "southamerica-east1-a"
-  instance_network = google_compute_network.mynetwork.self_link
+  instance_network = google_compute_network.mynetwork1.self_link
   instance_subnet  = google_compute_subnetwork.subnet-southamerica-east1.self_link
   instance_network_tag = "web"
   need_external_ip = true
 }
 
-module "database-vm" {
-  source           = "./instance"
-  instance_name    = "database-vm"
-  instance_zone    = "southamerica-east1-a"
-  instance_network = google_compute_network.mynetwork.self_link
-  instance_subnet  = google_compute_subnetwork.subnet-southamerica-east1.self_link
-  instance_network_tag = "database"
-}
  
 module "build-vm" {
   source           = "./instance"
   instance_name    = "build-vm"
   instance_zone    = "southamerica-east1-a"
-  instance_network = google_compute_network.mynetwork.self_link
+  instance_network = google_compute_network.mynetwork1.self_link
   instance_subnet  = google_compute_subnetwork.subnet-southamerica-east1.self_link
   instance_network_tag = "build"
 }
